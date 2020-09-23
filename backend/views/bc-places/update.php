51baafb387684eccc9b29dfb1dfb2ddb45c152ed
tvk777 <klimenko.tv7@gmail.com>
2020-09-23 15:18:06 +0300

    fix error update bc-paces

--- backend/views/bc-places/update.php
+++ backend/views/bc-places/update.php
@@ -12,7 +12,6 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Bc Places'), 'url' =
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 
-debug($model->getAllPrices());
 ?>
 <div class="bc-places-update">
 
